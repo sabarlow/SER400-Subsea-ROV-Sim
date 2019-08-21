35af184f109ff537c89d06728db2d885a25625a9
shaunbarlow <shaunbarlowmusic@gmail.com>
2019-08-21 23:07:52 +1000

    Added ROV control: W: forward S: backward mouse move: pitch & yaw Arrow keys: move manipulator Left click: close jaws (continuously rotates for now)
    Created MainScene with spotlights on ROV
    Made overhead ligth have blue colour

---
+++ Assets/Scripts/rovMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rovMove : MonoBehaviour
+{
+    public GameObject character;
+    public float speed;
+    public float turnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5f;
+        turnSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            character.transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.Self);
+            //Vector3 newPosition = 
+            //newPosition.z++;
+            //character.transform.position = newPosition;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            character.transform.Translate(Vector3.back * Time.deltaTime * speed, Space.Self);
+            //Vector3 newPosition = character.transform.position;
+            //newPosition.z--;
+            //character.transform.position = newPosition;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            character.transform.Rotate(Vector3.down * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            character.transform.Rotate(-Vector3.down * Time.deltaTime * speed);
+        }
+
+        float mousePitch = Input.GetAxis("Mouse Y");
+        character.transform.Rotate(Vector3.right * mousePitch);
+
+        float mouseYaw = Input.GetAxis("Mouse X");
+        character.transform.Rotate(Vector3.up * mouseYaw);
+    }
+}
